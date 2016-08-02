1884578f0aa5a9b7f6f67d516bcb549abc985778
Richard <cyc123bzd@gmail.com>
2016-08-03 01:39:16 +0800

    Remove element in arrays(Leetcode No.27) with linear TC.

---
+++ removeElement.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	
+    int removeElement(vector<int>& nums, int val) {
+        
+        int p = 0;
+        int v = 0;
+        for(int i=0; i<nums.size(); i++) {
+        	if(nums.at(i) == val) {
+        		v++;
+        	} else {
+        		p ++;
+        		nums.at(i-v) = nums.at(i);
+        	}
+        }
+        return p;
+    }
+};
+
+int main() {
+	
+	int arr[] = {1, 2, 2, 3, 5, 2, 4, 8, 2};
+	vector<int> a(arr, arr + sizeof(arr)/sizeof(int));
+	
+	Solution s;
+	int r = s.removeElement(a, 2);
+	
+	cout << r << endl;
+	for(int i=0; i<r; i++) {
+		cout << " " << a.at(i);
+	}
+}
